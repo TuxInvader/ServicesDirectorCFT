8081b02e7a656a57a5054fa58c4508d3fd321629
Mark Boddington <tuxinvader@users.noreply.github.com>
2017-07-17 14:55:29 +0100

    fix jq string

--- sd-deploy.sh
+++ sd-deploy.sh
@@ -102,9 +102,9 @@ remove_old_managers() {
     rest_user=$1
     rest_pass=$2
     sd_host=$3
-    for manager in $(curl -s -k -u "${rest_user}:${rest_pass}" https://localhost:8100/api/tmcm/2.5/manager | jq ".children | select(.[].name != '${sd_host}') | .[].href'"
+    for manager in $(curl -s -k -u "${rest_user}:${rest_pass}" https://localhost:8100/api/tmcm/2.5/manager | jq '.children' | jq ".[] | select(.name != \"${sd_host}\") | .href")
     do 
-        curl -s -k -u "${rest_user}:${rest_pass}" -X delete https://localhost:8100$manager
+        curl -s -k -u "${rest_user}:${rest_pass}" -X delete "https://localhost:8100${manager}"
     done
 }
 
